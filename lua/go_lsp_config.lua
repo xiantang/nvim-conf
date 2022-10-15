5fa77b757245aeaf28cb54e20ea8ea676d81dfea
xiantang <zhujingdi1998@gmail.com>
2022-10-15 15:51:10 +0800

    update config

--- lua/go_lsp_config.lua
+++ lua/go_lsp_config.lua
@@ -150,3 +150,21 @@ nvim_lsp.gopls.setup{
     end
   end
 
+local configs = require 'lspconfig/configs'
+
+
+if not configs.golangcilsp then
+ 	configs.golangcilsp = {
+		default_config = {
+			cmd = {'golangci-lint-langserver'},
+			root_dir = nvim_lsp.util.root_pattern('.git', 'go.mod'),
+			init_options = {
+					command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json", "--issues-exit-code=1" };
+			}
+		};
+	}
+end
+nvim_lsp.golangci_lint_ls.setup {
+	filetypes = {'go','gomod'}
+}
+
