7d2f0265784805cb6a4ab08b68495580f2a13713
xiantang <zhujingdi1998@gamil.com>
2022-10-19 23:40:44 +0800

    fix

---
+++ lua/conf.lua
@@ -0,0 +1,21 @@
+
+local map = require("utils").map
+
+
+
+function ToggleConfig()
+  print("ToggleConfig")
+  -- if already have config window, close it
+  if vim.fn.bufwinnr("init.vim") ~= -1 then
+    vim.cmd("q")
+    return
+  end
+  -- edit the config file
+  vim.cmd("split ~/.config/nvim/init.vim")
+end
+
+
+
+map("n", "<leader>e", ":lua ToggleConfig()<CR>,{silent = true}")
+
+
