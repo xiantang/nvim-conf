2f29ba02472b5a26a99018204e0adecb612f78c2
xiantang <zhujingdi1998@gmail.com>
2022-10-22 19:03:38 +0800

    update config

--- lua/go_lsp_config.lua
+++ lua/go_lsp_config.lua
@@ -248,10 +248,10 @@ cmp.setup {
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
       local copilot_keys = vim.fn['copilot#Accept']()
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
+      if luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
+      elseif cmp.visible() then
+        cmp.select_next_item()
       elseif copilot_keys ~= '' and type(copilot_keys) == 'string' then
         vim.api.nvim_feedkeys(copilot_keys, 'i', true)
       else
