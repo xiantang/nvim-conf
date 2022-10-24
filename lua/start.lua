691bcda7601c5bb91bf1bcb9be06ff4b005a13c6
xiantang <zhujingdi1998@gamil.com>
2022-10-24 18:38:31 +0800

    update config

--- lua/start.lua
+++ lua/start.lua
@@ -2,11 +2,14 @@ local db = require('dashboard')
 
 
 function RecentlySeesion()
+  -- pwd
   require("persistence").load({ last = true })
+  local pwd = vim.fn.getcwd()
   -- get current cursor position_self
   local cursor = vim.api.nvim_win_get_cursor(0)
+  local cmd = string.format("NERDTreeToggle %s | wincmd p", pwd)
   -- NerdTree
-  vim.cmd("NERDTreeToggle | wincmd p")
+  vim.cmd(cmd)
   -- move cursor to last position_self
   vim.api.nvim_win_set_cursor(0, cursor)
 end
