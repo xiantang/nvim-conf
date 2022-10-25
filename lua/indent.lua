d56e84046a3383ced1a7008067a8698f2dc4a32e
xiantang <zhujingdi1998@gmail.com>
2022-10-25 23:21:29 +0800

    update config

--- lua/indent.lua
+++ lua/indent.lua
@@ -1,5 +1,6 @@
 vim.opt.list = true
-vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "eol:↲"
+vim.opt.listchars:append "tab:» "
 require("indent_blankline").setup {
   show_end_of_line = true,
 }
