4689eb39d5774c25507bcbc4d39505c47bfdefae
xiantang <zhujingdi1998@gamil.com>
2022-10-26 01:43:39 +0800

    update config

--- lua/dap_set.lua
+++ lua/dap_set.lua
@@ -7,28 +7,45 @@ dap.defaults.fallback.terminal_win_cmd = '50vsplit new'
 dap.defaults.fallback.console = 'internalConsole'
 
 dap.listeners.after["event_initialized"]["key_map"] = function()
+  -- close nerd tree
+  vim.cmd("NERDTreeClose")
   require("dapui").open()
-  vim.api.nvim_buf_set_keymap(0, 'n', 'c', '<cmd>lua require"dap".continue()<CR>', { noremap = true, silent = true })
-  vim.api.nvim_buf_set_keymap(0, 'n', 'n', '<cmd>lua require"dap".step_over()<CR>', { noremap = true, silent = true })
-  vim.api.nvim_buf_set_keymap(0, 'n', 's', '<cmd>lua require"dap".step_into()<CR>', { noremap = true, silent = true })
-  vim.api.nvim_buf_set_keymap(0, 'n', 'o', '<cmd>lua require"dap".step_out()<CR>', { noremap = true, silent = true })
-  -- r
-  vim.api.nvim_buf_set_keymap(0, 'n', 'r', '<cmd>lua require"dap".repl.open()<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', 'c', '<cmd>lua require"dap".continue()<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', 'n', '<cmd>lua require"dap".step_over()<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', 's', '<cmd>lua require"dap".step_into()<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', 'o', '<cmd>lua require"dap".step_out()<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', 'r', '<cmd>lua require"dap".repl.open()<CR>', { noremap = true, silent = true })
+  -- stop terminal
+  vim.api.nvim_set_keymap('n', 'q', '<cmd>lua require"dap".disconnect()<CR>', { noremap = true, silent = true })
 
 
 end
 
+dap.listeners.after['disconnected']['key_map'] = function()
+  require("dapui").close()
+  vim.cmd("NERDTreeToggle | wincmd p")
+  -- rollback to default keymap
+  -- nvim_del_keymap
+  vim.api.nvim_del_keymap('n', 'c')
+  vim.api.nvim_del_keymap('n', 'n')
+  vim.api.nvim_del_keymap('n', 's')
+  vim.api.nvim_del_keymap('n', 'o')
+  vim.api.nvim_del_keymap('n', 'r')
+  vim.api.nvim_del_keymap('n', 'q')
+end
 
 -- attach stop
 
 dap.listeners.after["event_terminated"]["key_map"] = function()
   require("dapui").close()
+  vim.cmd("NERDTreeToggle | wincmd p")
   -- rollback to default keymap
-  -- nvim_buf_del_keymap
-  vim.api.nvim_buf_del_keymap(0, 'n', 'c')
-  vim.api.nvim_buf_del_keymap(0, 'n', 'n')
-  vim.api.nvim_buf_del_keymap(0, 'n', 's')
-  vim.api.nvim_buf_del_keymap(0, 'n', 'o')
-  vim.api.nvim_buf_del_keymap(0, 'n', 'r')
+  -- nvim_del_keymap
+  vim.api.nvim_del_keymap( 'n', 'c')
+  vim.api.nvim_del_keymap( 'n', 'n')
+  vim.api.nvim_del_keymap( 'n', 's')
+  vim.api.nvim_del_keymap( 'n', 'o')
+  vim.api.nvim_del_keymap( 'n', 'r')
+  vim.api.nvim_del_keymap( 'n', 'q')
 
 end
