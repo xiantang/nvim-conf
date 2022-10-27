37be1e8d38afd560419047e50ab8a5d395d7720b
xiantang <zhujingdi1998@gamil.com>
2022-10-27 23:20:17 +0800

    update config

--- lua/tele.lua
+++ lua/tele.lua
@@ -1,16 +1,13 @@
-require('telescope').setup{
+require('telescope').setup {
   extensions = {
-  frecency = {
-    show_scores = true,
-    show_unindexed = true,
-    ignore_patterns = {"*.git/*", "*/tmp/*"},
-    disable_devicons = false,
-    workspaces = {
-      ["conf"]    = "/home/my_username/.config",
-      ["data"]    = "/home/my_username/.local/share",
-      ["project"] = "/home/my_username/projects",
-      ["wiki"]    = "/home/my_username/wiki"
+    frecency = {
+      show_scores = true,
+      show_unindexed = true,
+      ignore_patterns = {
+        "*.git/*",
+        "*/tmp/*"
+      },
+      disable_devicons = false,
     }
-  }
-},
+  },
 }
