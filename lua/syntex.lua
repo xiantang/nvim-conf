3500ca12acb126396d1ebc040e74fdb3e10be371
xiantang <zhujingdi1998@gamil.com>
2022-10-29 01:45:13 +0800

    update config

--- lua/syntex.lua
+++ lua/syntex.lua
@@ -1,9 +1,16 @@
+require("vim.treesitter.query").set_query("go", "highlight", "")
 require 'nvim-treesitter.configs'.setup {
   highlight = {
-    enable = false,
+    -- disable highlight for variable and package in file
+    enable = true,
     additional_vim_regex_highlighting = false,
   },
   indent = {
     enable = true,
   },
 }
+
+-- override variable  as white
+vim.cmd [[
+  hi! link TSVariable Normal
+]]
