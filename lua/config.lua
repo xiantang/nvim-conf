66dfda89cb36ea85e0a21d2c35cef2ac8c2d4247
xiantang <zhujingdi1998@gamil.com>
2022-10-30 23:27:10 +0800

    feat(scope): remove vim-go depend

--- lua/config.lua
+++ lua/config.lua
@@ -1,4 +1,24 @@
 require('toggle')
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+require("null-ls").setup({
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.format()
+        end,
+      })
+    end
+  end,
+  sources = {
+    require("null-ls").builtins.formatting.goimports
+  },
+})
 require('glow').setup()
 require("lualine_setup")
 require("luasnip.loaders.from_vscode").lazy_load()
