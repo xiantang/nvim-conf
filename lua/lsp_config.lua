ed10242df86740fb4cd4c6599ac9d602f18dd62c
xiantang <zhujingdi1998@gamil.com>
2022-10-31 00:51:21 +0800

    update config

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -1,7 +1,4 @@
-local nvim_lsp = require('lspconfig')
-
-
-
+local nvim_lsp = require("lspconfig")
 
 local golang_capabilities = vim.lsp.protocol.make_client_capabilities()
 golang_capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -46,50 +43,55 @@ function _G.debug_cur_test_func()
 end
 
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
 
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
 
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
   -- change hold time
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover,
-    {
-      border = "single",
-      focusable = false,
-      max_width = 80,
-      max_height = 20,
-    }
-  )
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = "single",
+    focusable = false,
+    max_width = 80,
+    max_height = 20,
+  })
   -- cousor hold for 3 seconds, show signature helper
   -- silent
   -- vim.api.nvim_command [[autocmd CursorHold  <buffer> lua vim.lsp.buf.hover() ]]
   -- Mappings.
   local opts = { noremap = true, silent = true }
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>Telescope lsp_definitions<CR>', opts)
-  buf_set_keymap('n', 'ga', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  buf_set_keymap("n", "gd", "<Cmd>Telescope lsp_definitions<CR>", opts)
+  buf_set_keymap("n", "ga", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
   -- coode action for extract function or variable
-  buf_set_keymap('v', 'ga', "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>", opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap("v", "ga", "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>", opts)
+  buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
 
-  buf_set_keymap('n', '<space>gi', '<cmd>Telescope lsp_implementations<CR>', opts)
-  buf_set_keymap('n', '<space>dt', "<cmd>lua require('dap-go').debug_test()<CR>", opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>gr', '<cmd>Telescope lsp_references<CR>', opts)
+  buf_set_keymap("n", "<space>gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+  buf_set_keymap("n", "<space>dt", "<cmd>lua require('dap-go').debug_test()<CR>", opts)
+  buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+  buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+  buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_set_keymap("n", "<space>gr", "<cmd>Telescope lsp_references<CR>", opts)
   -- buf_set_keymap('n', '<space>f', '<cmd>lua vim.diagnostic.set_loclist()<CR>', opts)
   -- if current buff end with _test.go, then set keymap for error
   local buf_name = vim.api.nvim_buf_get_name(bufnr)
   if string.find(buf_name, "_test.go$") then
-    buf_set_keymap('n', 'ge', '<cmd>lua vim.diagnostic.goto_next({severity = { min = vim.diagnostic.severity.WARN}})<CR>'
-      , opts)
+    buf_set_keymap(
+      "n",
+      "ge",
+      "<cmd>lua vim.diagnostic.goto_next({severity = { min = vim.diagnostic.severity.WARN}})<CR>",
+      opts
+    )
   else
-    buf_set_keymap('n', 'ge',
-      '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap("n", "ge", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
   end
 
   -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
@@ -103,7 +105,8 @@ local on_attach = function(client, bufnr)
 
   -- Set autocommands conditional on server_capabilities
   if client.server_capabilities.document_highlight then
-    vim.api.nvim_exec([[
+    vim.api.nvim_exec(
+      [[
       hi LspReferenceRead cterm=bold ctermbg=DarkMagenta guibg=LightYellow
       hi LspReferenceText cterm=bold ctermbg=DarkMagenta guibg=LightYellow
       hi LspReferenceWrite cterm=bold ctermbg=DarkMagenta guibg=LightYellow
@@ -112,10 +115,11 @@ local on_attach = function(client, bufnr)
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
-    ]], false)
+    ]] ,
+      false
+    )
   end
 end
-
 
 -- auto cmd
 
@@ -127,53 +131,56 @@ function gofumpt(timeoutms)
   vim.cmd(command)
 end
 
-local auto_format_regrexs = {
-  "*.lua",
-  "*.json",
-  "*.dockerfile",
-}
+-- local auto_format_regrexs = {
+--   "*.lua",
+--   "*.json",
+--   "*.dockerfile",
+-- }
 
-for _, regrex in ipairs(auto_format_regrexs) do
-  vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = regrex,
-    command = "lua vim.lsp.buf.format { async = false} ",
-  })
-end
+-- for _, regrex in ipairs(auto_format_regrexs) do
+--   vim.api.nvim_create_autocmd("BufWritePost", {
+--     pattern = regrex,
+--     command = "lua vim.lsp.buf.format { async = false} ",
+--   })
+-- end
 
-
-local lsp_configs = require 'lspconfig/configs'
-
+local lsp_configs = require("lspconfig/configs")
 
 if not lsp_configs.golangcilsp then
   lsp_configs.golangcilsp = {
     default_config = {
-      cmd = { 'golangci-lint-langserver' },
-      root_dir = nvim_lsp.util.root_pattern('.git', 'go.mod'),
+      cmd = { "golangci-lint-langserver" },
+      root_dir = nvim_lsp.util.root_pattern(".git", "go.mod"),
       init_options = {
-        command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json",
-          "--issues-exit-code=1" };
-      }
-    };
+        command = {
+          "golangci-lint",
+          "run",
+          "--enable-all",
+          "--disable",
+          "lll",
+          "--out-format",
+          "json",
+          "--issues-exit-code=1",
+        },
+      },
+    },
   }
 end
 
-
-
-nvim_lsp.golangci_lint_ls.setup {
-  filetypes = { 'go', 'gomod' }
-}
+nvim_lsp.golangci_lint_ls.setup({
+  filetypes = { "go", "gomod" },
+})
 -- set up lspconfig
-require("mason").setup {
+require("mason").setup({
   ui = {
     icons = {
-      package_installed = "✓"
-    }
-  }
-}
-require("mason-lspconfig").setup {
+      package_installed = "✓",
+    },
+  },
+})
+require("mason-lspconfig").setup({
   ensure_installed = { "sumneko_lua" },
-}
-
+})
 
 local common_servers = {
   "sqlls",
@@ -190,24 +197,23 @@ local common_servers = {
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 for _, server in pairs(common_servers) do
-  nvim_lsp[server].setup {
+  nvim_lsp[server].setup({
     on_attach = on_attach,
     capabilities = capabilities,
-  }
+  })
 end
 
-
-nvim_lsp.sumneko_lua.setup {
+nvim_lsp.sumneko_lua.setup({
   on_attach = on_attach,
   settings = {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
+        version = "LuaJIT",
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = { "vim" },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
@@ -218,12 +224,11 @@ nvim_lsp.sumneko_lua.setup {
         enable = false,
       },
     },
-  }
-}
+  },
+})
 
-
-nvim_lsp.gopls.setup {
-  cmd = { 'gopls' },
+nvim_lsp.gopls.setup({
+  cmd = { "gopls" },
   -- for postfix snippets and analyzers
   capabilities = golang_capabilities,
   settings = {
@@ -237,33 +242,32 @@ nvim_lsp.gopls.setup {
     },
   },
   on_attach = on_attach,
-}
+})
 
-
-local luasnip = require 'luasnip'
+local luasnip = require("luasnip")
 
 -- nvim-cmp setup
-local cmp = require 'cmp'
+local cmp = require("cmp")
 
 if cmp == nil then
   return
 end
 
-cmp.setup {
+cmp.setup({
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -271,8 +275,8 @@ cmp.setup {
       else
         fallback()
       end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
@@ -280,11 +284,11 @@ cmp.setup {
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { "i", "s" }),
   }),
   sources = {
-    { name = 'luasnip' },
-    { name = 'nvim_lsp' },
-    { name = 'path' },
+    { name = "luasnip" },
+    { name = "nvim_lsp" },
+    { name = "path" },
   },
-}
+})
