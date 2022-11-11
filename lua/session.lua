b6842483a475170878b9b5a7a4c62889ad05260b
xiantang <zhujingdi1998@gamil.com>
2022-11-11 15:42:50 +0800

    feat(scope): GenReturn for method

--- lua/session.lua
+++ lua/session.lua
@@ -1,7 +1,4 @@
-require("persistence").setup {
+require("persistence").setup({
   dir = vim.fn.expand(vim.fn.stdpath("config") .. "/sessions/"), -- directory where session files are saved
-  options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
-}
-
-
-
+  options = { "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
+})
