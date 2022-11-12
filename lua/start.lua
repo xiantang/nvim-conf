7c05556d010b454aaa4234510110b4e4673ee724
xiantang <zhujingdi1998@gamil.com>
2022-11-12 21:17:03 +0800

    feat(scope): add scoll bar

--- lua/start.lua
+++ lua/start.lua
@@ -1,19 +1,15 @@
 function RecentlySeesion()
-  -- pwd
-  require("persistence").load({ last = true })
-  local pwd = vim.fn.getcwd()
-  -- get current cursor position_self
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local cmd = string.format("NERDTreeToggle %s | wincmd p", pwd)
-  -- NerdTree
-  vim.cmd(cmd)
-  -- move cursor to last position_self
-  vim.api.nvim_win_set_cursor(0, cursor)
-  vim.cmd("MinimapToggle")
-  -- buffer enter events
-  -- if it's Minimap buffer, wincmd p to move to last buffer
-  vim.cmd("autocmd BufEnter * if bufname('%') =~ 'Minimap' | wincmd p | endif")
-  -- file type
+	-- pwd
+	require("persistence").load({ last = true })
+	local pwd = vim.fn.getcwd()
+	-- get current cursor position_self
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	local cmd = string.format("NERDTreeToggle %s | wincmd p", pwd)
+	-- NerdTree
+	vim.cmd(cmd)
+	-- move cursor to last position_self
+	vim.api.nvim_win_set_cursor(0, cursor)
+	-- buffer enter events
 end
 
 -- https://github.com/neovim/neovim/issues/8136#issuecomment-373082539 nested
