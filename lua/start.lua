86d17d58d4ffec086fb5cef7bcada8af4dc346f8
xiantang <zhujingdi1998@gamil.com>
2022-11-13 15:20:58 +0800

    feat(snippet): add more snippets

--- lua/start.lua
+++ lua/start.lua
@@ -1,16 +1,16 @@
 function RecentlySeesion()
-	-- pwd
-	require("persistence").load({ last = true })
-	local pwd = vim.fn.getcwd()
-	-- get current cursor position_self
-	local cursor = vim.api.nvim_win_get_cursor(0)
-	local cmd = string.format("NERDTreeToggle %s | wincmd p", pwd)
-	-- NerdTree
-	vim.cmd(cmd)
-	-- move cursor to last position_self
-	vim.api.nvim_win_set_cursor(0, cursor)
-	-- buffer enter events
+  -- pwd
+  require("persistence").load({ last = true })
+  local pwd = vim.fn.getcwd()
+  -- get current cursor position_self
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local cmd = string.format("NERDTreeToggle %s | wincmd p", pwd)
+  -- NerdTree
+  vim.cmd(cmd)
+  -- move cursor to last position_self
+  vim.api.nvim_win_set_cursor(0, cursor)
+  -- buffer enter events
 end
 
 -- https://github.com/neovim/neovim/issues/8136#issuecomment-373082539 nested
-vim.cmd("autocmd VimEnter * nested lua RecentlySeesion()")
+vim.cmd("silent! autocmd VimEnter *  nested lua RecentlySeesion()")
