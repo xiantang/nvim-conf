4792935c7831041d59294f486ed007e2613b70dc
xiantang <zhujingdi1998@gmail.com>
2022-11-15 00:05:34 +0800

    feat(scope): reset

--- reset.sh
+++ reset.sh
@@ -2,7 +2,7 @@
  
 git filter-branch -f --env-filter '
  
-OLD_EMAIL="zhujingdi1998@gamil.com"
+OLD_EMAIL="jingdizhu@jingdis-MBP.lan"
 CORRECT_NAME="xiantang"
 CORRECT_EMAIL="zhujingdi1998@gmail.com"
 
