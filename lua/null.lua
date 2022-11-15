a8f86833fadbfdf553692af844b9ff0d52f83efe
xiantang <zhujingdi1998@gmail.com>
2022-11-15 23:12:03 +0800

    feat(scope): add spell check

--- lua/null.lua
+++ lua/null.lua
@@ -1,20 +1,20 @@
 local null_ls = require("null-ls")
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.nginx_beautifier,
-    null_ls.builtins.formatting.goimports,
-    null_ls.builtins.code_actions.shellcheck,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.jq,
-    null_ls.builtins.diagnostics.hadolint,
-  },
+	sources = {
+		null_ls.builtins.formatting.nginx_beautifier,
+		null_ls.builtins.formatting.goimports,
+		null_ls.builtins.code_actions.shellcheck,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.jq,
+		null_ls.builtins.diagnostics.hadolint,
+	},
 })
 
 local function golines()
-  -- get current file path
-  local file_path = vim.api.nvim_buf_get_name(0)
-  local command = string.format("!golines -m 400 -w --no-reformat-tags %s", file_path)
-  -- run cmd in backgroundColor
-  -- run in async
-  vim.cmd(command)
+	-- get current file path
+	local file_path = vim.api.nvim_buf_get_name(0)
+	local command = string.format("!golines -m 400 -w --no-reformat-tags %s", file_path)
+	-- run cmd in backgroundColor
+	-- run in async
+	vim.cmd(command)
 end
