13b72fb8aaa24eea2475d2eb2f534c12e899a5ad
xiantang <zhujingdi1998@gmail.com>
2022-11-25 00:54:12 +0800

    set db_safe_mode as false

--- lua/tele.lua
+++ lua/tele.lua
@@ -1,14 +1,15 @@
-require('telescope').setup {
-  extensions = {
-    frecency = {
-      show_scores = true,
-      show_unindexed = true,
-      ignore_patterns = {
-        "*.git/*",
-        "*/tmp/*"
-      },
-      disable_devicons = false,
-    }
-  },
-}
--- require("telescope").load_extension("refactoring")
+require("telescope").setup({
+	extensions = {
+		frecency = {
+			db_safe_mode = false,
+			show_scores = true,
+			show_unindexed = true,
+			ignore_patterns = {
+				"*.git/*",
+				"*/tmp/*",
+			},
+			disable_devicons = false,
+		},
+	},
+})
+require("telescope").load_extension("frecency")
