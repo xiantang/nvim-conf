3cafc155b5c02afba8e3d0e5f086556c9a704d9b
xiantang <zhujingdi1998@gmail.com>
2022-11-27 17:47:08 +0800

    commit

--- lua/start.lua
+++ lua/start.lua
@@ -1,15 +1,5 @@
 function RecentlySeesion()
 	require("persistence").load({ last = true })
-	local pwd = vim.fn.getcwd()
-	-- -- get current cursor position_self
-	local cursor = vim.api.nvim_win_get_cursor(0)
-	-- get current win
-	local win = vim.api.nvim_get_current_win()
-	local cmd = string.format("NERDTreeToggle %s", pwd)
-	-- -- NerdTree
-	vim.cmd(cmd)
-	-- move cursor to last position_self
-	vim.api.nvim_win_set_cursor(win, cursor)
 end
 
 -- https://github.com/neovim/neovim/issues/8136#issuecomment-373082539 nested
--- lua/term.lua
+++ lua/term.lua
@@ -1,18 +1,21 @@
 require("toggleterm").setup({
-  start_in_insert = false,
-  on_open = function(_)
-    -- check nerdtree is visible
-    local name = vim.fn.bufname("NERD_tree_")
-    -- get buff id
-    local id = vim.fn.bufnr(name)
-    if id ~= -1 then
-      local pwd = vim.fn.getcwd()
-      vim.defer_fn(function()
-        local cmd = string.format("NERDTreeToggle %s ", pwd)
-        vim.cmd(cmd)
-        local cmd_retoggle = string.format("NERDTreeToggle %s | 1 | wincmd p", pwd)
-        vim.cmd(cmd_retoggle)
-      end, 100)
-    end
-  end,
+	start_in_insert = false,
+	on_open = function(_)
+		-- check nerdtree is visible
+		local name = vim.fn.bufname("NERD_tree_")
+		if name == "" then
+			return
+		end
+		-- get buff id
+		local id = vim.fn.bufnr(name)
+		if id ~= -1 then
+			local pwd = vim.fn.getcwd()
+			vim.defer_fn(function()
+				local cmd = string.format("NERDTreeToggle %s ", pwd)
+				vim.cmd(cmd)
+				local cmd_retoggle = string.format("NERDTreeToggle %s | 1 | wincmd p", pwd)
+				vim.cmd(cmd_retoggle)
+			end, 100)
+		end
+	end,
 })
