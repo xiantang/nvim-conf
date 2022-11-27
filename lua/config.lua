5d7fe6919a37642df9e8582bf5dcae87e9eed60f
xiantang <zhujingdi1998@gmail.com>
2022-11-27 18:34:12 +0800

    feat(scope): reslove conflict

--- lua/config.lua
+++ lua/config.lua
@@ -65,3 +65,16 @@ vim.cmd([[silent! colorscheme base16-tender]])
 vim.cmd([[
   hi! link TSVariable Normal
 ]])
+
+vim.cmd([[
+      let g:conflict_marker_highlight_group = ''
+      " Include text after begin and end markers
+      let g:conflict_marker_begin = '^<<<<<<< .*$'
+      let g:conflict_marker_end   = '^>>>>>>> .*$'
+
+      highlight ConflictMarkerBegin guibg=#2f7366
+      highlight ConflictMarkerOurs guibg=#2e5049
+      highlight ConflictMarkerTheirs guibg=#344f69
+      highlight ConflictMarkerEnd guibg=#2f628e
+      highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
+]])
