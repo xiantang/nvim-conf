02de37f4161a50cb52228abd664ce1c29effbe9c
xiantang <zhujingdi1998@gmail.com>
2022-11-27 17:53:56 +0800

    fix(scope): term

--- lua/term.lua
+++ lua/term.lua
@@ -3,11 +3,18 @@ require("toggleterm").setup({
 	on_open = function(_)
 		-- check nerdtree is visible
 		local name = vim.fn.bufname("NERD_tree_")
-		if name == "" then
-			return
-		end
 		-- get buff id
 		local id = vim.fn.bufnr(name)
+		-- get win by buff id
+		local win = vim.fn.win_findbuf(id)
+		-- length of win
+		local count = 0
+		for _, _ in ipairs(vim.fn.win_findbuf(id)) do
+			count = count + 1
+		end
+		if count == 0 then
+			return
+		end
 		if id ~= -1 then
 			local pwd = vim.fn.getcwd()
 			vim.defer_fn(function()
