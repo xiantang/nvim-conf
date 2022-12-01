7d24fb3802011f683d6702615049af2bfafec20f
xiantang <zhujingdi1998@gmail.com>
2022-12-01 09:49:01 +0800

    feat(scope): add tabnine

--- lua/cmp_set.lua
+++ lua/cmp_set.lua
@@ -63,7 +63,7 @@ cmp.setup({
 	sources = {
 		{ name = "luasnip", priority = 100 },
 		{ name = "nvim_lsp", priority = 99, max_item_count = 10 },
-		{ name = "cmp_tabnine", priority = 98 },
+		{ name = "cmp_tabnine", priority = 98, max_item_count = 2, keyword_length = 3 },
 		{ name = "buffer", priority = 80, max_item_count = 3, keyword_length = 3 },
 		{ name = "path", priority = 80, max_item_count = 3, keyword_length = 3 },
 		-- disable fuzzy
