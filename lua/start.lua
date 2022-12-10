40fc97a343a1fb82a429803e3a5c54720e8852e6
xiantang <zhujingdi1998@gmail.com>
2022-12-10 21:11:52 +0800

    feat(scope): remove unused code

--- lua/start.lua
+++ lua/start.lua
@@ -1,12 +1,4 @@
 M = {}
-function RecentlySeesion()
-	-- check filenvim
-	if vim.g.started_by_firenvim == true then
-		-- set font size
-		return
-	end
-	require("persistence").load({ last = true })
-end
 
 -- https://github.com/neovim/neovim/issues/8136#issuecomment-373082539 nested
 vim.cmd("silent! autocmd VimEnter *  nested lua RecentlySeesion()")
