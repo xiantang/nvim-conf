887181a7ba9c60f90a1bbc27551fa93cde00af72
xiantang <zhujingdi1998@gmail.com>
2023-01-09 14:38:52 +0800

    feat(scope): add configure of specs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -56,7 +56,25 @@ return require("packer").startup(function(use)
 	lock_use({
 		"edluffy/specs.nvim",
 		config = function()
-			require("specs").setup({})
+			require("specs").setup({
+				{
+					show_jumps = true,
+					min_jump = 30,
+					popup = {
+						delay_ms = 0, -- delay before popup displays
+						inc_ms = 10, -- time increments used for fade/resize effects
+						blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
+						width = 5,
+						winhl = "PMenu",
+						fader = require("specs").linear_fader,
+						resizer = require("specs").shrink_resizer,
+					},
+					ignore_filetypes = {},
+					ignore_buftypes = {
+						nofile = true,
+					},
+				},
+			})
 		end,
 	})
 	lock_use({ "j-hui/fidget.nvim" })
