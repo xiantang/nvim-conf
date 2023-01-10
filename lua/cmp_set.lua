2518ab52cd5e252621d19e9a2ef654e6d2137447
xiantang <zhujingdi1998@gmail.com>
2023-01-11 01:02:08 +0800

    feat(scope): use <C-c> to abort the compeletion

--- lua/cmp_set.lua
+++ lua/cmp_set.lua
@@ -34,6 +34,8 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
+		-- abort
+		["<C-c>"] = cmp.mapping.abort(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
