cb27c3d7ab62675d08016075efae574efa14bbbe
xiantang <zhujingdi1998@gmail.com>
2023-01-18 22:53:11 +0800

    feat(scope): add lualine copilot

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,9 @@
 return {
 	event = "VeryLazy",
 	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		{ "ofseed/lualine-copilot" },
+	},
 	config = function()
 		local colors = {
 			white = "#abb2bf",
@@ -17,7 +20,55 @@ return {
 			red = "#ec5f67",
 			grey = "#3e4452",
 		}
+		local ok, lazy_status = pcall(require, "lazy.status")
+		if not ok then
+			vim.notify("Could not load lazy.status")
+			return
+		end
+		local function indent()
+			if vim.o.expandtab then
+				return "SW:" .. vim.o.shiftwidth
+			else
+				return "TS:" .. vim.o.tabstop
+			end
+		end
+		local function lsp()
+			local clients = vim.lsp.buf_get_clients()
+			if #clients == 0 then
+				return ""
+			end
+			local names = {}
+			local ignored = { "null-ls", "copilot" }
+			for _, client in ipairs(clients) do
+				if not vim.tbl_contains(ignored, client.name) then
+					table.insert(names, client.name)
+				end
+			end
+			local msg = table.concat(names, ", ")
+			if msg == "" then
+				return ""
+			else
+				return " " .. msg
+			end
+		end
 		require("lualine").setup({
+			sections = {
+				lualine_c = {
+					lsp,
+				},
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					"copilot",
+					"filetype",
+					indent,
+					"encoding",
+					"fileformat",
+				},
+			},
 			options = {
 				theme = {
 					normal = {
