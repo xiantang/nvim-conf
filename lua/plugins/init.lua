11972bdab15c4666654c2d570dfee9a565abeca0
xiantang <zhujingdi1998@gmail.com>
2023-01-18 22:29:24 +0800

    feat(scope): use cmd to traggle playground

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -338,7 +338,7 @@ return {
 		event = "VeryLazy",
 		commit = "a60af980b6f0a6e7ee239ffb9d1d519aaaa1de58",
 	},
-	{ "nvim-treesitter/playground", event = "VeryLazy" },
+	{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
 	{
 		"L3MON4D3/LuaSnip",
 		commit = "79f647218847b1cd204fede7dd89025e43fd00c3",
