dea0a72c2498b9a1bd6122df0256d0f8c49acbc2
xiantang <zhujingdi1998@gmail.com>
2023-01-18 16:36:33 +0800

    feat(scope): commit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -122,7 +122,6 @@ return {
 	{
 		"kylechui/nvim-surround",
 		event = "VeryLazy",
-		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
 		config = function()
 			require("nvim-surround").setup({
 				-- Configuration here, or leave empty to use defaults
