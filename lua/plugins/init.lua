056404363b37660518ab03547026ec6590ff96e0
xiantang <zhujingdi1998@gmail.com>
2023-02-10 17:18:21 +0800

    feat(scope): add git

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -174,23 +174,6 @@ return {
 		event = "VeryLazy",
 	},
 	{
-		"tpope/vim-fugitive",
-		config = function() end,
-		cmd = "Git",
-	},
-	{
-		"shumphrey/fugitive-gitlab.vim",
-		event = "VeryLazy",
-		config = function()
-			-- check operation system
-			local is_mac = vim.fn.has("mac") == 1
-			if is_mac then
-				local secret = require("secret")
-				vim.g.fugitive_gitlab_domains = { secret.GITALB_URL }
-			end
-		end,
-	},
-	{
 		"segeljakt/vim-silicon",
 		event = "VeryLazy",
 		config = function()
