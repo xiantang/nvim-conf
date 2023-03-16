192ed217ec17a66bed4aac732670fb191b6b7592
xiantang <zhujingdi1998@gmail.com>
2023-03-16 13:00:47 +0800

    Add Lua Telescope colors picker feature and comment out lsp-lens plugin in lsp.lua configuration.

---
+++ lua/my_extension.lua
@@ -0,0 +1,29 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+-- our picker function: colors
+local colors = function(opts)
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = "colors",
+    finder = finders.new_table {
+      results = { "red", "green", "blue" }
+    },
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        -- print(vim.inspect(selection))
+        vim.api.nvim_put({ selection[1] }, "", false, true)
+      end)
+      return true
+    end,
+  }):find()
+end
+
+-- to execute the function
+-- colors(require("telescope.themes").get_dropdown{})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -40,15 +40,15 @@ return {
 			require("lsp_config")
 		end,
 	},
-	{
-		"VidocqH/lsp-lens.nvim",
-		event = "VeryLazy",
-		ft = { "go" },
-		dev = true,
-		config = function()
-			require("lsp-lens").setup()
-		end,
-	},
+	-- {
+	-- 	"VidocqH/lsp-lens.nvim",
+	-- 	event = "VeryLazy",
+	-- 	ft = { "go" },
+	-- 	dev = true,
+	-- 	config = function()
+	-- 		require("lsp-lens").setup()
+	-- 	end,
+	-- },
 	{ "williamboman/mason.nvim", event = "VeryLazy" },
 	{ "williamboman/mason-lspconfig.nvim", event = "VeryLazy" },
 	{ "glepnir/lspsaga.nvim", commit = "b7b4777", event = "VeryLazy" },
