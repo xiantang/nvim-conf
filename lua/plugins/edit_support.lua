3cfaea31b826ef974650fc6c2ea3479063f39214
xiantang <zhujingdi1998@gmail.com>
2023-03-16 22:20:09 +0800

    Add vim-silicon to edit_support.lua and remove unnecessary plugins from init.lua.

--- lua/plugins/edit_support.lua
+++ lua/plugins/edit_support.lua
@@ -1,6 +1,16 @@
 return {
 	{ "romainl/vim-cool", event = "VeryLazy" },
 	{
+		"segeljakt/vim-silicon",
+		event = "VeryLazy",
+		config = function()
+			vim.cmd([[
+			 let g:silicon={}
+			 let g:plug_window = 'botright 40vnew'
+			 let g:silicon['output'] = '~/images/silicon-{time:%Y-%m-%d-%H%M%S}.png']])
+		end,
+	},
+	{
 		event = "VeryLazy",
 		"andymass/vim-matchup",
 		config = function()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,1 @@
-return {
-	{
-		"segeljakt/vim-silicon",
-		event = "VeryLazy",
-		config = function()
-			vim.cmd([[
-			 let g:silicon={}
-			 let g:plug_window = 'botright 40vnew'
-			 let g:silicon['output'] = '~/images/silicon-{time:%Y-%m-%d-%H%M%S}.png']])
-		end,
-	},
-	{ "romainl/vim-cool", event = "VeryLazy" },
-}
+return {}
