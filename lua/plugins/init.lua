0db99fb7df84e679c9dc5f80010732f755b3d819
xiantang <zhujingdi1998@gmail.com>
2023-03-16 22:10:12 +0800

    Remove indent plugins.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -76,25 +76,6 @@ return {
 		end,
 	},
 	{ "romainl/vim-cool", event = "VeryLazy" },
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			vim.opt.list = true
-			vim.opt.listchars:append("eol:↲")
-			require("indent_blankline").setup({
-				show_end_of_line = true,
-			})
-		end,
-		event = "VeryLazy",
-	},
-	{
-		"echasnovski/mini.indentscope",
-		version = false,
-		config = function()
-			require("mini.indentscope").setup({})
-		end,
-		event = "VeryLazy",
-	},
 	{ "vim-scripts/ReplaceWithRegister", event = "VeryLazy" },
 	{
 		"folke/neodev.nvim",
