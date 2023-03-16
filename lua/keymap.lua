4ddbddba6a5de6f6bf2bef1b6c58758a43d3b6fc
xiantang <zhujingdi1998@gmail.com>
2023-03-16 21:21:00 +0800

    Add smart location functionality for jumping between projects using Telescope picker.

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -75,63 +75,6 @@ cnoremap <D-v> <c-r>+
 ]])
 
 vim.keymap.set("n", "<C-q>", ":Telescope oldfiles<CR>", {})
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
-local conf = require("telescope.config").values
-local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
-
-project_picker = function(opts)
-	local bookmarks = vim.fn.readfile(vim.env.HOME .. "/.NERDTreeBookmarks")
-	local choices = {}
-
-	for i, bookmark in ipairs(bookmarks) do
-		local path = vim.split(bookmark, " ")[2]
-		if path ~= nil then
-			table.insert(choices, path)
-		end
-	end
-	opts = opts or {}
-	pickers
-		.new(opts, {
-			prompt_title = "Jump Between Projects",
-			finder = finders.new_table({
-				results = choices,
-			}),
-			sorter = conf.generic_sorter(opts),
-			attach_mappings = function(prompt_bufnr, map)
-				actions.select_default:replace(function()
-					actions.close(prompt_bufnr)
-					local selection = action_state.get_selected_entry()
-					-- print(vim.inspect(selection))
-					require("telescope.builtin").find_files({
-						-- exclude png files
-						file_ignore_patterns = { "*.png", "*.ttf", ".git" },
-						search_dirs = { selection.value },
-						-- show hidden files
-						hidden = true,
-						attach_mappings = function(prompt_bufnr, map)
-							actions.select_default:replace(function()
-								actions.close(prompt_bufnr)
-								local selection = action_state.get_selected_entry()
-								-- print(vim.inspect(selection))
-								vim.cmd("e " .. selection.value)
-								vim.cmd("NerdSmartLocated")
-								vim.cmd("wincmd p")
-							end)
-							return true
-						end,
-					})
-				end)
-				return true
-			end,
-		})
-		:find()
-end
-
--- to execute the function
--- bind to a key
-vim.keymap.set("n", "<C-e>", ":lua project_picker(require('telescope.themes').get_dropdown{})<CR>", { silent = true })
 vim.cmd([[
 nnoremap <Leader>[  <C-O>
 nnoremap <Leader>]  <C-I>
