8a8d8200f42a24ddd40a7c8f7ce1a0b174f9501e
xiantang <zhujingdi1998@gmail.com>
2023-03-31 09:47:35 +0800

    Added event "VeryLazy" to treesitter plugin and removed edit_support context plugin.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter-context",
+		event = "VeryLazy",
 		config = function()
 			require("treesitter-context").setup({
 				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
