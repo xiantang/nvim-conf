2b73a4f78b7e1cd7e178fa20c721ca7ae1163d21
xiantang <zhujingdi1998@gmail.com>
2023-04-02 13:29:34 +0800

    Add Harpoon plugin for quick file navigation and commenting out unused code in motion and txtobjs plugins. Also, add Harpoon extension for Telescope plugin.

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -1,30 +1,5 @@
 return {
 	{
-		event = "VeryLazy",
-		"edluffy/specs.nvim",
-		config = function()
-			require("specs").setup({
-				{
-					show_jumps = true,
-					min_jump = 30,
-					popup = {
-						delay_ms = 0, -- delay before popup displays
-						inc_ms = 10, -- time increments used for fade/resize effects
-						blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
-						width = 5,
-						winhl = "PMenu",
-						fader = require("specs").linear_fader,
-						resizer = require("specs").shrink_resizer,
-					},
-					ignore_filetypes = {},
-					ignore_buftypes = {
-						nofile = true,
-					},
-				},
-			})
-		end,
-	},
-	{
 		"jinh0/eyeliner.nvim",
 		event = "VeryLazy",
 		config = function()
