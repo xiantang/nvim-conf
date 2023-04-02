541f95cc9d1dbd41121f2d1d7b00addd39f119bc
xiantang <zhujingdi1998@gmail.com>
2023-04-02 13:39:51 +0800

    Refactor keyboard shortcuts for Harpoon in browser.lua file. Replaced <Leader>1, <Leader>2, and <Leader>3 with <C-m>.

--- lua/plugins/browser.lua
+++ lua/plugins/browser.lua
@@ -43,9 +43,7 @@ endif
 			{ "<C-n>", ':lua require("harpoon.ui").nav_next()<CR>', {} },
 			{ "<C-p>", ':lua require("harpoon.ui").nav_prev()<CR>', {} },
 			{ "<Leader>a", ':lua require("harpoon.mark").add_file()<CR>', {} },
-			{ "<Leader>1", ':lua require("harpoon.ui").nav_file(1)<CR>', {} },
-			{ "<Leader>2", ':lua require("harpoon.ui").nav_file(2)<CR>', {} },
-			{ "<Leader>3", ':lua require("harpoon.ui").nav_file(3)<CR>', {} },
+			{ "<C-m>", ':lua require("harpoon.ui").nav_file(1)<CR>', {} },
 		},
 		config = function()
 			require("harpoon").setup({})
