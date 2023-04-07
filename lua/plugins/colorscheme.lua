c91acd9bdeb0bfa9ec8a3fd7a860c5f835eae3ba
xiantang <zhujingdi1998@gmail.com>
2023-04-08 00:55:53 +0700

    Add cursor highlighting and color scheme updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,6 +7,15 @@ return {
 			local c = require("nordic.colors")
 			require("nordic").setup({
 				override = {
+					LineNr = {
+						fg = c.white0,
+					}, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
+					CursorLineNr = {
+						fg = c.yellow.base,
+					}, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
+					VertSplit = {
+						fg = c.blue1,
+					}, -- the column separating vertically split windows
 					WinSeparator = {
 						fg = c.blue1,
 						bold = true,
