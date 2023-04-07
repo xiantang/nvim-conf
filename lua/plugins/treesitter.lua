b2bbd48bc7a1f62a97c3aab48b363d1ea59d3a10
xiantang <zhujingdi1998@gmail.com>
2023-04-08 01:02:26 +0700

    Add color scheme options to treesitter plugin in lua/plugins/treesitter.lua.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -46,6 +46,14 @@ return {
 				},
 				rainbow = {
 					enable = true,
+					colors = {
+						"#a89984",
+						"#b16286",
+						"#d79921",
+						"#689d6a",
+						"#d65d0e",
+						"#458588",
+					},
 					-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
 					extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
 					max_file_lines = nil, -- Do not enable for files with more than n lines, int
