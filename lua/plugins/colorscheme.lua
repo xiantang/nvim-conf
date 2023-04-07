b1e63bcc357414fadb29bd71f9e30993b003cd53
xiantang <zhujingdi1998@gmail.com>
2023-04-07 23:54:50 +0700

    Update colorscheme to nordic and lualine theme to nordic as well. Add nordic.nvim plugin as well.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,12 @@
 return {
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nordic").load()
+		end,
+	},
 	{ "catppuccin/nvim", name = "catppuccin", lazy = true },
 	{
 		"RRethy/nvim-base16",
