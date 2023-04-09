ecd469b91c3a2f3a22fecc3d970c292611449c6b
xiantang <zhujingdi1998@gmail.com>
2023-04-09 18:43:22 +0800

    Comment out colorscheme plugins in lua/plugins/colorscheme.lua.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -25,13 +25,13 @@ return {
 			require("nordic").load()
 		end,
 	},
-	{ "catppuccin/nvim", name = "catppuccin", lazy = true },
-	{
-		"RRethy/nvim-base16",
-		lazy = true,
-	},
-	{
-		"sainnhe/sonokai",
-		lazy = true,
-	},
+	-- { "catppuccin/nvim", name = "catppuccin", lazy = true },
+	-- {
+	-- 	"RRethy/nvim-base16",
+	-- 	lazy = true,
+	-- },
+	-- {
+	-- 	"sainnhe/sonokai",
+	-- 	lazy = true,
+	-- },
 }
