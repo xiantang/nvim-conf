28738a2e33adb8fdf83e4dae56acef4254212d78
xiantang <zhujingdi1998@gmail.com>
2023-04-17 11:55:43 +0800

    Update .luarc.json and lazy-lock.json, remove unnecessary code from lua/plugins/ui.lua and lua/lsp_config.lua, and add a line to lua/plugins/telescope.lua.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
 		keys = {
 			{ "<C-e>", ":lua project_picker(require('telescope.themes').get_dropdown{})<CR>", desc = "smart location" },
 			{ "<Leader>p", ":Telescope find_files<CR>", {} },
+			{ "<Leader>o", ":Telescope lsp_document_symbols<CR>", {} },
 			{ "<Leader>P", ":Telescope live_grep<CR>", {} },
 			{ "<C-q>", ":Telescope oldfiles<CR>", {} },
 		},
