4e4c02f6bf7689aeca72d230e98c8cd44f63478b
xiantang <zhujingdi1998@gmail.com>
2023-04-18 22:40:53 +0800

    Refactor triggers in lua/plugins/which-key.lua.

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,7 +6,18 @@ return {
 			vim.o.timeout = true
 			vim.o.timeoutlen = 400
 			require("which-key").setup({
-				triggers = { "<leader>", '"' },
+				triggers = {
+					-- marks
+					"<leader>",
+					"`",
+					"g`",
+					"g'",
+					-- registers
+					'"',
+					"<c-r>",
+					-- spelling
+					"z=",
+				},
 				-- your configuration comes here
 				-- or leave it empty to use the default settings
 				-- refer to the configuration section below
