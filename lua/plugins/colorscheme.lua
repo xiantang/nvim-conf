ebb9c8c18e831738b7ca288b420b554ee587ee7b
xiantang <zhujingdi1998@gmail.com>
2023-04-23 14:11:05 +0800

    Removed echasnovski/mini.colors from colorscheme.lua.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,4 @@
 return {
-	{
-		"echasnovski/mini.colors",
-		config = function()
-			require("mini.colors").setup()
-		end,
-	},
 	{ "catppuccin/nvim", name = "catppuccin", lazy = true },
 	{
 		"RRethy/nvim-base16",
