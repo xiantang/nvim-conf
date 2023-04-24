e4e9a0cd836c5b9bd308e3cf8b0348a6d6fb4db6
xiantang <zhujingdi1998@gmail.com>
2023-04-25 00:37:46 +0800

    Add global variable to allow different key for Harftime plugin.

--- init.lua
+++ init.lua
@@ -27,6 +27,7 @@ vim.g.hardtime_maxcount = 2
 vim.g.hardtime_timeout = 500
 vim.g.list_of_normal_keys = { "h", "j", "k", "l", "-", "+", "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>" }
 vim.g.list_of_visual_keys = { "h", "j", "k", "l", "-", "+", "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>" }
+vim.g.hardtime_allow_different_key = 1
 vim.g.list_of_insert_keys = { "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>" }
 vim.g.list_of_disabled_keys = {}
 vim.g.mapleader = " "
