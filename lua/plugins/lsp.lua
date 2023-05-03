027169a85cfc8c2f13032703535fb18c6d0a9faa
xiantang <zhujingdi1998@gmail.com>
2023-05-03 10:27:29 +0800

    Add shfmt formatting with null_ls in lsp plugin.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,6 +6,7 @@ return {
 			local null_ls = require("null-ls")
 			null_ls.setup({
 				sources = {
+					null_ls.builtins.formatting.shfmt,
 					null_ls.builtins.formatting.nginx_beautifier,
 					null_ls.builtins.formatting.gofumpt,
 					null_ls.builtins.code_actions.shellcheck,
