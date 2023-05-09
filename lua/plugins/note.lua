7b8ace1d60be24a9645a4200294a7fa38dff1a11
xiantang <zhujingdi1998@gmail.com>
2023-05-09 15:06:39 +0800

    Updated the 'core.concealer' plugin configuration in note.lua to disable folds.

--- lua/plugins/note.lua
+++ lua/plugins/note.lua
@@ -5,7 +5,11 @@ return {
 		opts = {
 			load = {
 				["core.defaults"] = {}, -- Loads default behaviour
-				["core.concealer"] = {}, -- Adds pretty icons to your documents
+				["core.concealer"] = {
+					config = {
+						folds = false,
+					},
+				}, -- Adds pretty icons to your documents
 				["core.dirman"] = { -- Manages Neorg workspaces
 					config = {
 						workspaces = {
