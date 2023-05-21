575dce5fcac4386e259e8b64a26d00b163e31863
xiantang <zhujingdi1998@gmail.com>
2023-05-21 15:05:10 +0800

    Add function to get name of current class definition or declaration in Lua using nvim-treesitter.

--- lua/dap_set.lua
+++ lua/dap_set.lua
@@ -221,3 +221,60 @@ function _G.function_surrounding_cursor()
 	prev_function_name = find_name(func)
 	return prev_function_name
 end
+
+local prev_class_node = nil
+local prev_class_name = ""
+
+function _G.class_surrounding_cursor()
+	local ts_utils = require("nvim-treesitter.ts_utils")
+	local current_node = ts_utils.get_node_at_cursor()
+
+	if not current_node then
+		return ""
+	end
+
+	local func = current_node
+
+	while func do
+		if func:type() == "class_definition" or func:type() == "class_declaration" then
+			break
+		end
+
+		func = func:parent()
+	end
+
+	if not func then
+		prev_class_node = nil
+		prev_class_name = ""
+		return ""
+	end
+
+	if func == prev_class_node then
+		return prev_class_name
+	end
+
+	prev_class_node = func
+
+	local find_name
+	find_name = function(node)
+		for i = 0, node:named_child_count() - 1, 1 do
+			local child = node:named_child(i)
+			local type = child:type()
+
+			if type == "identifier" or type == "operator_name" then
+				return (ts_utils.get_node_text(child))[1]
+			else
+				local name = find_name(child)
+
+				if name then
+					return name
+				end
+			end
+		end
+
+		return nil
+	end
+
+	prev_class_name = find_name(func)
+	return prev_class_name
+end
