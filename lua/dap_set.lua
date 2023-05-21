64936403ddcf78cebe64dfd300576cf02610b9d0
xiantang <zhujingdi1998@gmail.com>
2023-05-21 13:44:16 +0800

    Add log-to-file function and update Python debugging configurations.

--- lua/dap_set.lua
+++ lua/dap_set.lua
@@ -108,3 +108,47 @@ end
 dap.listeners.after["event_terminated"]["key_map"] = function()
 	defer()
 end
+
+local function log_to_file()
+	-- https://github.com/microsoft/debugpy/wiki/Enable-debugger-logs
+	vim.env.DEBUGPY_LOG_DIR = vim.fn.stdpath("cache") .. "/debugpy"
+	return true
+end
+
+dap.configurations.python = {
+	{
+		type = "python",
+		request = "launch",
+		name = "Launch file",
+		program = "${file}",
+		logToFile = log_to_file,
+		pythonPath = function()
+			return "/opt/homebrew/bin/python3"
+		end,
+	},
+}
+dap.adapters.python = function(cb, config)
+	if config.request == "attach" then
+		---@diagnostic disable-next-line: undefined-field
+		local port = (config.connect or config).port
+		---@diagnostic disable-next-line: undefined-field
+		local host = (config.connect or config).host or "127.0.0.1"
+		cb({
+			type = "server",
+			port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+			host = host,
+			options = {
+				source_filetype = "python",
+			},
+		})
+	else
+		cb({
+			type = "executable",
+			command = "python",
+			args = { "-m", "debugpy.adapter" },
+			options = {
+				source_filetype = "python",
+			},
+		})
+	end
+end
