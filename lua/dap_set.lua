b12a4c6405a13f8b4201496b17cb0f8915b88ddf
xiantang <zhujingdi1998@gmail.com>
2023-05-21 14:53:13 +0800

    Add function to retrieve name of function surrounding cursor in lua/dap_set.lua

--- lua/dap_set.lua
+++ lua/dap_set.lua
@@ -163,3 +163,61 @@ dap.adapters.python = function(cb, config)
 		})
 	end
 end
+
+local prev_function_node = nil
+local prev_function_name = ""
+
+-- < Retrieve the name of the function the cursor is in.
+function _G.function_surrounding_cursor()
+	local ts_utils = require("nvim-treesitter.ts_utils")
+	local current_node = ts_utils.get_node_at_cursor()
+
+	if not current_node then
+		return ""
+	end
+
+	local func = current_node
+
+	while func do
+		if func:type() == "function_definition" or func:type() == "function_declaration" then
+			break
+		end
+
+		func = func:parent()
+	end
+
+	if not func then
+		prev_function_node = nil
+		prev_function_name = ""
+		return ""
+	end
+
+	if func == prev_function_node then
+		return prev_function_name
+	end
+
+	prev_function_node = func
+
+	local find_name
+	find_name = function(node)
+		for i = 0, node:named_child_count() - 1, 1 do
+			local child = node:named_child(i)
+			local type = child:type()
+
+			if type == "identifier" or type == "operator_name" then
+				return (ts_utils.get_node_text(child))[1]
+			else
+				local name = find_name(child)
+
+				if name then
+					return name
+				end
+			end
+		end
+
+		return nil
+	end
+
+	prev_function_name = find_name(func)
+	return prev_function_name
+end
