c6825376a5a42e695bf68f0e715c1841ac365f48
xiantang <zhujingdi1998@gmail.com>
2023-05-21 23:54:31 +0800

    Add bash debugger adapter configuration for easier debugging of bash scripts.

--- lua/dap_set.lua
+++ lua/dap_set.lua
@@ -311,3 +311,32 @@ dap.configurations.python = {
 		end,
 	},
 }
+
+dap.adapters.bashdb = {
+	type = "executable",
+	command = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/bash-debug-adapter",
+	name = "bashdb",
+	pathBash = "/opt/homebrew/bin/bash",
+}
+
+dap.configurations.sh = {
+	{
+		type = "bashdb",
+		request = "launch",
+		name = "Launch file",
+		showDebugOutput = true,
+		pathBashdb = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
+		pathBashdbLib = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
+		trace = true,
+		file = "${file}",
+		program = "${file}",
+		cwd = "${workspaceFolder}",
+		pathCat = "cat",
+		pathBash = "/opt/homebrew/bin/bash",
+		pathMkfifo = "mkfifo",
+		pathPkill = "pkill",
+		args = {},
+		env = {},
+		terminalKind = "integrated",
+	},
+}
