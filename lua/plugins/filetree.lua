eb38cf133e13ee1d4ac64dcbfd5ed008606a17d2
xiantang <zhujingdi1998@gmail.com>
2023-05-27 11:23:02 +0800

    Add file visibility options in filetree plugin view.

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -6,6 +6,19 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			local fm = require("oil").setup({
+
+				view_options = {
+					-- Show files and directories that start with "."
+					show_hidden = true,
+					-- This function defines what is considered a "hidden" file
+					is_hidden_file = function(name, bufnr)
+						return vim.startswith(name, ".")
+					end,
+					-- This function defines what will never be shown, even when `show_hidden` is set
+					is_always_hidden = function(name, bufnr)
+						return false
+					end,
+				},
 				keymaps = {
 					["g?"] = "actions.show_help",
 					["<CR>"] = "actions.select",
