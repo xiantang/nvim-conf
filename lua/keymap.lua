8ea9fc8c1501d196503eb2276318b77407e458a4
xiantang <zhujingdi1998@gmail.com>
2023-05-29 23:06:23 +0800

    Update keymap.lua to map "`a" to "<Tab>a" in normal mode.

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -14,7 +14,7 @@ vim.keymap.set("n", "<Leader>[", "<C-O>", opts)
 vim.keymap.set("n", "<Leader>]", "<C-I>", opts)
 
 vim.keymap.set("n", "<Tab>", "`", opts)
-vim.keymap.set("n", "`a", "`azz", opts)
+vim.keymap.set("n", "<Tab>a", "`azz", opts)
 vim.keymap.set("n", "o", "o", opts)
 vim.keymap.set("n", "c", "c", opts)
 vim.keymap.set("n", "r", "r", opts)
