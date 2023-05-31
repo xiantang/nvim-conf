09e88b226a47b5988745a7a07f0cafc2572aa0cb
xiantang <zhujingdi1998@gmail.com>
2023-05-31 17:34:30 +0800

    Refactor keymap.lua to use Lua functions instead of Vimscript expressions for j and k mappings

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -7,8 +7,22 @@
 -- ]])
 local opts = { noremap = true, silent = true }
 -- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
-vim.keymap.set("n", "j", [[(v:count > 1 ? "m'" . v:count : '') . 'gj']], { noremap = true, expr = true })
-vim.keymap.set("n", "k", [[(v:count > 1 ? "m'" . v:count : '') . 'gk']], { noremap = true, expr = true })
+vim.keymap.set("n", "j", function()
+	-- if count > 1 then
+	if vim.v.count > 1 then
+		return "m'" .. vim.v.count .. "j"
+	else
+		return "gj"
+	end
+end, { noremap = true, expr = true })
+vim.keymap.set("n", "k", function()
+	-- if count > 1 then
+	if vim.v.count > 1 then
+		return "m'" .. vim.v.count .. "k"
+	else
+		return "gk"
+	end
+end, { noremap = true, expr = true })
 
 vim.keymap.set("n", "<Leader>[", "<C-O>", opts)
 vim.keymap.set("n", "<Leader>]", "<C-I>", opts)
