bf7fa4d9efa456f7e5967aeb15f0d6c23f294e68
xiantang <zhujingdi1998@gmail.com>
2023-06-04 20:44:54 +0800

    Update key mapping for Vim: Change the key binding from Ctrl+C to Ctrl+P.

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -16,7 +16,7 @@ vim.keymap.set("v", "y", "ygv<esc>", opts)
 
 -- nnoremap <c-c> :cp<CR>
 -- nnoremap <c-n> :cn<CR>
-vim.keymap.set("n", "<c-c>", ":cp<CR>", opts)
+vim.keymap.set("n", "<c-p>", ":cp<CR>", opts)
 vim.keymap.set("n", "<c-n>", ":cn<CR>", opts)
 -- becase hhkb layout backstick is hard to press
 vim.keymap.set("n", "<Tab><Tab>", "``zz", opts)
