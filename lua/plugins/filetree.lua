04a9916c5e19489d0f3dd76b234d89394be02ff8
xiantang <zhujingdi1998@gmail.com>
2023-06-06 21:34:23 +0800

    Add custom key mappings to nvim-tree plugin attachment function.

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -11,7 +11,22 @@ return {
 			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
+			local function my_on_attach(bufnr)
+				local api = require("nvim-tree.api")
+
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				-- default mappings
+				api.config.mappings.default_on_attach(bufnr)
+
+				-- custom mappings
+				vim.keymap.set("n", "s", api.node.open.vertical, opts("Vsplit"))
+				vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+			end
 			require("nvim-tree").setup({
+				on_attach = my_on_attach,
 				sort_by = "case_sensitive",
 				view = {
 					width = 30,
