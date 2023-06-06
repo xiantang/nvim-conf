d4ab7114d51b1c8170bc4c36b65fd1be100b5da7
xiantang <zhujingdi1998@gmail.com>
2023-06-06 20:42:23 +0800

    Add nvim-tree.lua as a dependency and configure filetree.lua to use it

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,5 +1,15 @@
 return {
 	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
+	{
 		"stevearc/oil.nvim",
 		opts = {},
 		-- Optional dependencies
