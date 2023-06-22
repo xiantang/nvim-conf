affa258017a451ac7a71df175aedf5fde09e746f
xiantang <zhujingdi1998@gmail.com>
2023-06-22 16:35:10 +0800

    Add other.nvim plugin and update txtobjs.lua and go.json snippets.

--- lua/plugins/txtobjs.lua
+++ lua/plugins/txtobjs.lua
@@ -24,13 +24,12 @@ return {
 							-- You can use the capture groups defined in textobjects.scm
 							["af"] = "@function.outer",
 							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-
+							["ic"] = "@conditional.inner",
+							["ac"] = "@conditional.outer",
 							["aa"] = "@parameter.outer",
 							["ia"] = "@parameter.inner",
 							-- You can optionally set descriptions to the mappings (used in the desc parameter of
 							-- nvim_buf_set_keymap) which plugins like which-key display
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
 							-- You can also use captures from other query groups like `locals.scm`
 							["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
 						},
