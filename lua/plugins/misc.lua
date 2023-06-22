c27d9bea2a54540d95ec50ffe71ce5859e503f97
xiantang <zhujingdi1998@gmail.com>
2023-06-22 10:20:16 +0800

    Add flash.nvim to plugins/misc.lua options for lazy loading.

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,1 +1,39 @@
-return {}
+return {
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {
+			modes = {
+				char = {
+					enabled = true,
+					search = { wrap = false },
+					highlight = { backdrop = true },
+					jump = { register = false },
+				},
+			},
+		},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					-- default options: exact mode, multi window, all directions, with a backdrop
+					require("flash").jump({
+						search = { forward = true, wrap = false, multi_window = false },
+					})
+				end,
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					-- default options: exact mode, multi window, all directions, with a backdrop
+					require("flash").jump({
+						search = { forward = false, wrap = false, multi_window = false },
+					})
+				end,
+			},
+		},
+	},
+}
