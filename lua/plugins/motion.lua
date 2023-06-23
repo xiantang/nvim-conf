2202a71ccc74c7ca2b438c729bbfb9939b7f755c
xiantang <zhujingdi1998@gmail.com>
2023-06-24 00:28:57 +0800

    Remove commented out Harpoon configuration in motion plugin.

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -8,21 +8,4 @@ return {
 			})
 		end,
 	},
-	-- {
-	-- 	"ThePrimeagen/harpoon",
-	-- 	lazy = true,
-	-- 	config = function()
-	-- 		-- when instert start
-	-- 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	-- 			pattern = { "*.md", "*.go", "*.py", "*.lua", "*.sh" },
-	-- 			callback = function()
-	-- 				require("harpoon.mark").add_file()
-	-- 			end,
-	-- 		})
-	-- 		require("harpoon").setup({
-	-- 			excluded_filetypes = { "harpoon", "TelescopePrompt" },
-	-- 		})
-	-- 		-- code
-	-- 	end,
-	-- },
 }
