9e3ea6714e6fe84cf2e22c559c88a30849b4d94d
xiantang <zhujingdi1998@gmail.com>
2023-06-23 23:49:53 +0800

    Update harpoon and add keybind and config in misc.lua.

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,1 +1,16 @@
-return {}
+return {
+	{
+		"ThePrimeagen/harpoon",
+		keys = {
+			{
+				"<leader>m",
+				":lua require('harpoon.ui').toggle_quick_menu()<CR>",
+			},
+		},
+		config = function()
+			require("harpoon").setup({
+				save_on_change = true,
+			})
+		end,
+	},
+}
