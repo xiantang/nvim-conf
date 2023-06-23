1c4e7ca75fb96a6aebde96084dd79acf7b529459
xiantang <zhujingdi1998@gmail.com>
2023-06-23 23:57:24 +0800

    Add Harpoon keybindings for quickly navigating between files.

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -3,8 +3,32 @@ return {
 		"ThePrimeagen/harpoon",
 		keys = {
 			{
+				"<leader>a",
+				':lua require("harpoon.mark").add_file()<CR>',
+			},
+			{
 				"<leader>m",
 				":lua require('harpoon.ui').toggle_quick_menu()<CR>",
+			},
+			{
+				"<leader>1",
+				':lua require("harpoon.ui").nav_file(1)<CR>',
+			},
+			{
+				"<leader>2",
+				':lua require("harpoon.ui").nav_file(2)<CR>',
+			},
+			{
+				"<leader>3",
+				':lua require("harpoon.ui").nav_file(3)<CR>',
+			},
+			{
+				"<leader>4",
+				':lua require("harpoon.ui").nav_file(4)<CR>',
+			},
+			{
+				"<leader>5",
+				':lua require("harpoon.ui").nav_file(5)<CR>',
 			},
 		},
 		config = function()
