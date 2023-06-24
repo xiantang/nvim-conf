a1d1f2d2e79e50eff6371ace6a19c222eadee48d
xiantang <zhujingdi1998@gmail.com>
2023-06-24 16:09:12 +0800

    Fix typo in keymap.lua comment about HHKB layout.

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -16,7 +16,7 @@ vim.keymap.set("v", "y", "ygv<esc>", opts)
 -- nnoremap <c-n> :cn<CR>
 vim.keymap.set("n", "<c-p>", ":cp<CR>", opts)
 vim.keymap.set("n", "<c-n>", ":cn<CR>", opts)
--- becase hhkb layout backstick is hard to press
+-- because hhkb layout backtick is hard to press
 vim.keymap.set("n", "<Tab><Tab>", "``", opts)
 vim.keymap.set("n", "<Tab>a", "`azz", opts)
 vim.keymap.set("n", "<Tab>b", "`bzz", opts)
