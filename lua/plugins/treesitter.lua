534ba3446174d1109721cd97ff080212f0ab118f
xiantang <zhujingdi1998@gmail.com>
2023-07-02 15:09:01 +0800

    commit

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,43 +18,6 @@ return {
 			})
 		end,
 	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-
-				matchup = {
-					enable = true, -- mandatory, false will disable the whole extension
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						node_incremental = "v",
-						node_decremental = "<BS>",
-					},
-				},
-				highlight = {
-					-- disable highlight for variable and package in file
-					enable = true,
-					additional_vim_regex_highlighting = false,
-					disable = {},
-				},
-				indent = {
-					-- because it's buggy
-					enable = false,
-				},
-				rainbow = {
-					enable = true,
-					-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-					extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-					max_file_lines = nil, -- Do not enable for files with more than n lines, int
-					-- colors = {}, -- table of hex strings
-					-- termcolors = {} -- table of colour name strings
-				},
-			})
-		end,
-	},
 	-- { "mrjones2014/nvim-ts-rainbow", event = "VeryLazy" },
 	{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
 }
