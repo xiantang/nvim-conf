9b429fdbb3cd6b44505dd9206a578cf7749831f9
xiantang <zhujingdi1998@gmail.com>
2023-07-05 13:15:34 +0800

    Add gotmpl filetype to *.template files and configure treesitter parser for gotmpl files

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -6,6 +6,7 @@ au FileType * set formatoptions-=cro
 au FileType nerdtree setlocal relativenumber
 au CursorHold * checktime
 au BufRead,BufNewFile *.jq setfiletype jq
+au BufNewFile,BufRead *.template setfiletype gotmpl
 au BufWritePost *.go silent! :lua go_org_imports()
 au BufNewFile,BufRead */ssh/config  setf sshconfig
 au CursorHold,CursorHoldI * checktime
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -89,6 +89,15 @@ return {
 					},
 				},
 			})
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.gotmpl = {
+				install_info = {
+					url = "https://github.com/ngalaiko/tree-sitter-go-template",
+					files = { "src/parser.c" },
+				},
+				filetype = "gotmpl",
+				used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" },
+			}
 		end,
 	},
 	{
