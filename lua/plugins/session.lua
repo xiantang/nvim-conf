a0072bbe8c20608a613d018fc1851fedea14745c
xiantang <zhujingdi1998@gmail.com>
2023-07-16 12:26:15 +0800

    Remove unnecessary option 'blank' from 'sessionoptions' in session.lua

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"rmagatti/auto-session",
 		config = function()
-			vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+			vim.o.sessionoptions = "buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 			require("auto-session").setup({
 				log_level = "error",
 				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
