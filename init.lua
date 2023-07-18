2b32cd50f72d5c42870f73c218afbb7715455562
xiantang <zhujingdi1998@gmail.com>
2023-07-18 15:58:07 +0800

    Update font in init.lua

--- init.lua
+++ init.lua
@@ -6,15 +6,8 @@
 vim.cmd("source ~/.config/nvim/.vimrc")
 
 if vim.g.neovide then
-	local alpha = function()
-		return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
-	end
-	-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-	vim.g.neovide_transparency = 0.0
-	vim.g.transparency = 0.90
-	vim.g.neovide_background_color = "#0f1117" .. alpha()
 	-- Put anything you want to happen only in Neovide here
-	vim.o.guifont = "JetBrains Mono:h15:b"
+	vim.o.guifont = "FiraCode Nerd Font Mono:h16"
 end
 vim.g.hardtime_default_on = 1
 vim.g.hardtime_maxcount = 2
