033a748a2a5e8363008c7e799f9fb82d5a6f08aa
xiantang <zhujingdi1998@gmail.com>
2023-07-18 18:00:26 +0800

    Remove jq formatting from lsp.lua file.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,6 @@ return {
 					null_ls.builtins.formatting.gofumpt,
 					null_ls.builtins.code_actions.shellcheck,
 					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.jq,
 					-- for python
 					null_ls.builtins.formatting.black,
 					null_ls.builtins.formatting.isort,
