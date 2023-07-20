00771bb4152f59eacb32e21c837e0f34887c6c45
xiantang <zhujingdi1998@gmail.com>
2023-07-21 00:07:39 +0800

    Add LuaSnip plugin and configure snippet loaders
    Add LuaSnip plugin with commit 79f6472 and configure the snippet loaders for various file types. Also, include the "friendly-snippets" dependency.
    
    Add Go snippet for length function
    
    Add a Go snippet for the length function, transforming ".len" to "len()".

--- lua/plugins/snippet.lua
+++ lua/plugins/snippet.lua
@@ -4,6 +4,21 @@ return {
 		"hrsh7th/vim-vsnip-integ",
 	},
 	{
+		"L3MON4D3/LuaSnip",
+		commit = "79f647218847b1cd204fede7dd89025e43fd00c3",
+		event = "InsertEnter",
+		config = function()
+			require("luasnip.loaders.from_vscode").load({
+				include = { "norg", "c", "go", "python", "sh", "json", "lua", "gitcommit", "sql", "markdown" },
+			})
+			require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.expand("~/.config/nvim/my_snippets") } })
+			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/my_snippets/lua/" })
+		end,
+		dependencies = {
+			"xiantang/friendly-snippets",
+		},
+	},
+	{
 		"hrsh7th/vim-vsnip",
 		config = function()
 			vim.cmd([[
