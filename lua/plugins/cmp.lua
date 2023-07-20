17df657c56a2472f700229c637ddbde2e74cd816
xiantang <zhujingdi1998@gmail.com>
2023-07-21 00:42:47 +0800

    Refactor luasnip function call in cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -168,7 +168,7 @@ return {
 							cmp.select_next_item()
 						-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
 						-- they way you will only jump inside the snippet region
-						elseif luasnip.expand_or_jumpable() then
+						elseif luasnip.expand_or_locally_jumpable()() then
 							luasnip.expand_or_jump()
 						elseif has_words_before() then
 							cmp.complete()
