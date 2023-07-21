701cf3b458505a889830669b86b707c5ef0c8dc6
xiantang <zhujingdi1998@gmail.com>
2023-07-21 09:43:02 +0800

    Refactor has_words_before function to remove duplication and improve readability

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -13,12 +13,6 @@ return {
 				return
 			end
 			local lspkind = require("lspkind")
-			local has_words_before = function()
-				unpack = unpack or table.unpack
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
 
 			local feedkey = function(key, mode)
 				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
@@ -88,8 +82,6 @@ return {
 						-- they way you will only jump inside the snippet region
 						elseif vim.fn["vsnip#available"](1) == 1 then
 							feedkey("<Plug>(vsnip-expand-or-jump)", "")
-						elseif has_words_before() then
-							cmp.complete()
 						else
 							fallback()
 						end
@@ -157,8 +149,6 @@ return {
 						-- they way you will only jump inside the snippet region
 						elseif luasnip.expand_or_locally_jumpable()() then
 							luasnip.expand_or_jump()
-						elseif has_words_before() then
-							cmp.complete()
 						else
 							fallback()
 						end
