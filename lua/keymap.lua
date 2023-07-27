e4757265e1f467d7b8485c8d02dac320756961c4
xiantang <zhujingdi1998@gmail.com>
2023-07-27 22:58:34 +0800

    Refactor keymap.lua to fix issue with gj and gk keybindings.

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -6,9 +6,6 @@
 -- set smartindent
 -- ]])
 local opts = { noremap = true, silent = true }
--- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
--- vim.keymap.set("n", "j", [[v:count > 0 ? "m'" . v:count . "j" : 'gj']], { noremap = true, expr = true })
--- vim.keymap.set("n", "k", [[v:count > 0 ? "m'" . v:count . "k" : 'gk']], { noremap = true, expr = true })
 
 vim.keymap.set("v", "y", "ygv<esc>", opts)
 
