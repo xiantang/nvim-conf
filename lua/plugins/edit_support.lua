77add2f9fd920e7d0dc6efd61081bd1be9c564d6
xiantang <zhujingdi1998@gmail.com>
2023-08-03 23:33:13 +0800

    Refactor edit_support.lua to use "scrolloff" method for offscreen matching parentheses in vim.

--- lua/plugins/edit_support.lua
+++ lua/plugins/edit_support.lua
@@ -15,7 +15,7 @@ return {
 		"andymass/vim-matchup",
 		config = function()
 			-- may set any options here
-			vim.g.matchup_matchparen_offscreen = { method = "popup" }
+			vim.g.matchup_matchparen_offscreen = { method = "scrolloff" }
 		end,
 	},
 	{
