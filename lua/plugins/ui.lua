95839a9259f8a65f7468256af58486a7daa2b9d6
xiantang <zhujingdi1998@gmail.com>
2023-08-15 00:14:06 +0800

    Remove scrollbar handlers for gitsigns and related changes in ui.lua.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -24,29 +24,8 @@ return {
 		"petertriho/nvim-scrollbar",
 		event = "VeryLazy",
 		config = function()
-			require("scrollbar.handlers.gitsigns").setup()
 			require("scrollbar").setup({
 				set_highlights = true,
-				marks = {
-					GitAdd = {
-						text = "│",
-						priority = 5,
-						color = "green",
-						cterm = nil,
-					},
-					GitChange = {
-						text = "│",
-						priority = 5,
-						color = "yellow",
-						cterm = nil,
-					},
-					GitDelete = {
-						text = "_",
-						priority = 5,
-						color = "red",
-						cterm = nil,
-					},
-				},
 			})
 		end,
 	},
