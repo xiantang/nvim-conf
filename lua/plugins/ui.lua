f900ad47f49ac3cdeaca0758586d6ebdc5fd9b71
xiantang <zhujingdi1998@gmail.com>
2023-08-15 00:41:10 +0800

    Toggle cursor and handle handlers for UI plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -25,6 +25,14 @@ return {
 		event = "VeryLazy",
 		config = function()
 			require("scrollbar").setup({
+				handlers = {
+					cursor = true,
+					diagnostic = false,
+					gitsigns = false, -- Requires gitsigns
+					handle = true,
+					search = false, -- Requires hlslens
+					ale = false, -- Requires ALE
+				},
 				set_highlights = true,
 			})
 		end,
