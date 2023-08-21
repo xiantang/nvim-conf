6b1b05163d7650af7bf24809532f4216e079b9a6
jingdi.zhu <jing.zhu@shopee.com>
2023-08-21 10:00:22 +0800

    Update max_num_results in cmp.lua and refactor the shell snippet for index-based iteration in sh.json.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -28,7 +28,7 @@ return {
 
 			tabnine:setup({
 				max_lines = 1000,
-				max_num_results = 20,
+				max_num_results = 1,
 				sort = true,
 				run_on_every_keystroke = true,
 				snippet_placeholder = "..",
