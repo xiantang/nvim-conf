ac5dd025bd5a1ca96686f3495039e24b2c66bfc6
jingdi.zhu <jing.zhu@shopee.com>
2023-08-21 10:03:08 +0800

    Change kind mode from "symbol" to "symbol_text" in cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -57,7 +57,7 @@ return {
 					format = function(entry, vim_item)
 						-- if you have lspkind installed, you can use it like
 						-- in the following line:
-						vim_item.kind = lspkind.symbolic(vim_item.kind, { mode = "symbol" })
+						vim_item.kind = lspkind.symbolic(vim_item.kind, { mode = "symbol_text" })
 						vim_item.menu = menu[entry.source.name]
 						if entry.source.name == "cmp_tabnine" then
 							local detail = (entry.completion_item.labelDetails or {}).detail
