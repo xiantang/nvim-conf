dae33d2b91ab8e15a990d94c535d8182901aa4c3
jingdi.zhu <jing.zhu@shopee.com>
2023-08-24 22:20:51 +0800

    Add treesitter configuration to enable syntax highlighting for certain languages and disable it for others.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,24 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = {
+					enable = true,
+					disable = function(lang, buf)
+						local enabled = {
+							"gomod",
+							"gosum",
+						}
+						for _, enable in pairs(enabled) do
+							if lang == enable then
+								return false
+							end
+						end
+						return true
+					end,
+				},
+			})
+		end,
 	},
 }
