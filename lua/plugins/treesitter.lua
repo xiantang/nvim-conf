d24981cd99197d306b0ada0e344a4360bd4a6234
jingdi.zhu <jing.zhu@shopee.com>
2023-08-24 22:51:03 +0800

    Add support for sql and json syntax highlighting in treesitter.lua.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,12 +11,19 @@ return {
 				"make",
 				"gitignore",
 				"terraform",
+				"sql",
+				"json",
 			}
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = enabled,
 				highlight = {
 					enable = true,
 					disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
 						for _, enable in pairs(enabled) do
 							if lang == enable then
 								return false
