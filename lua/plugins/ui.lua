459d38f20be39911cd392199f0ae2e04512c39c9
jingdi.zhu <jing.zhu@shopee.com>
2023-09-02 23:55:06 +0800

    Remove unused plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,26 +1,5 @@
 return {
 	{
-		"rcarriga/nvim-notify",
-		config = function()
-			require("notify").setup({})
-		end,
-	},
-	{
-		"folke/zen-mode.nvim",
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
-	{
-		"simrat39/symbols-outline.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("symbols-outline").setup()
-		end,
-	},
-	{
 		"petertriho/nvim-scrollbar",
 		event = "VeryLazy",
 		config = function()
