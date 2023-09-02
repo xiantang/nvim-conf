d07834582449ceae77634056e199a3fc20753594
jingdi.zhu <jing.zhu@shopee.com>
2023-09-02 23:38:14 +0800

    Disable lightbulb code actions in lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -52,6 +52,9 @@ return {
 		config = function()
 			local saga = require("lspsaga")
 			saga.init_lsp_saga({
+				code_action_lightbulb = {
+					enabled = false,
+				},
 				finder_icons = {
 					def = "󰆧  ",
 					ref = "  ",
