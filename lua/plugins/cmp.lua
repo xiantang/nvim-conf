98d89afecc9ba697398608d5b1cbdd6a77617d6c
jingdi.zhu <jing.zhu@shopee.com>
2023-09-03 13:56:03 +0800

    Updated cmp-buffer dependency

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -212,7 +212,7 @@ return {
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-cmdline",
-			"ray-x/cmp-buffer",
+			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-vsnip",
 			"saadparwaiz1/cmp_luasnip",
