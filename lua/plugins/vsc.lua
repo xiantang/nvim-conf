76e6ec8975f7fcce508fe584c363f1657a79b0c0
xiantang <zhujingdi1998@gmail.com>
2023-09-28 09:32:33 +0800

    commit

--- lua/plugins/vsc.lua
+++ lua/plugins/vsc.lua
@@ -41,14 +41,14 @@ return {
 					follow_files = true,
 				},
 				attach_to_untracked = true,
-				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-				current_line_blame_opts = {
-					virt_text = true,
-					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-					delay = 1000,
-					ignore_whitespace = false,
-				},
-				current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+				-- current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				-- current_line_blame_opts = {
+				-- 	virt_text = true,
+				-- 	virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				-- 	delay = 1000,
+				-- 	ignore_whitespace = false,
+				-- },
+				-- current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
 				sign_priority = 6,
 				update_debounce = 101,
 				status_formatter = nil, -- Use default
