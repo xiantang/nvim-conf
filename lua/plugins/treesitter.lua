62788e81f6c419ea396760ebb8d89ad92d5ce8da
xiantang <zhujingdi1998@gmail.com>
2023-10-04 21:29:46 +0800

    commit

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -28,7 +28,7 @@ return {
 				incremental_selection = {
 					enable = true,
 					keymaps = {
-						init_selection = "v", -- set to `false` to disable one of the mappings
+						-- init_selection = "v", -- set to `false` to disable one of the mappings
 						node_incremental = "v",
 						-- scope_incremental = "grc",
 						node_decremental = "<BS>",
