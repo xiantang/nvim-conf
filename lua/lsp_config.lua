02e8150c923cea4b94971c3e448e79d8b385df77
xiantang <zhujingdi1998@gmail.com>
2023-10-17 12:30:18 +0800

    commit

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -162,6 +162,21 @@ end
 local lsp_configs = safeRequire("lspconfig/configs")
 
 -- set up lspconfig
+local is_mac = vim.fn.has("mac") == 1
+if is_mac == 0 then
+	local mason_registry = require("mason-registry")
+	mason_registry:on("package:install:success", function(pkg)
+		pkg:get_receipt():if_present(function(receipt)
+			for _, rel_path in pairs(receipt.links.bin) do
+				local bin_abs_path = pkg:get_install_path() .. "/extension/server/bin/" .. rel_path
+				os.execute(
+					'patchelf --set-interpreter "$(patchelf --print-interpreter $(grep -oE \\/nix\\/store\\/[a-z0-9]+-neovim-unwrapped-[0-9]+\\.[0-9]+\\.[0-9]+\\/bin\\/nvim $(which nvim)))" '
+						.. bin_abs_path
+				)
+			end
+		end)
+	end)
+end
 safeRequire("mason").setup({
 	ui = {
 		icons = {
