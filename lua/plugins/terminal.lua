f015437de4dbee63483e106d694d2d98a6d7f3f9
xiantang <zhujingdi1998@gmail.com>
2023-10-22 11:42:24 +0800

    commitj

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -6,6 +6,19 @@ return {
 			require("toggleterm").setup({
 				direction = "float",
 			})
+			local Terminal = require("toggleterm.terminal").Terminal
+			local lazygit = Terminal:new({ cmd = "sudo lazygit", hidden = true })
+
+			function _lazygit_toggle()
+				lazygit:toggle()
+			end
+
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>g",
+				"<cmd>lua _lazygit_toggle()<CR>",
+				{ noremap = true, silent = true }
+			)
 		end,
 	},
 }
