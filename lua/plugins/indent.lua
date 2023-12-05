5691aa37d29d85f2accb7a98fa8ecde6158709b6
xiantang <zhujingdi1998@gmail.com>
2023-12-05 23:23:09 +0800

    commit

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,8 +1,29 @@
 return {
 	{
-		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+		opts = {
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+			scope = { enabled = false },
+			exclude = {
+				filetypes = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+			},
+		},
 		main = "ibl",
-		opts = {},
 	},
 }
