f3aec02479d14828b84591f104301c9aa8878ab1
xiantang <zhujingdi1998@gmail.com>
2023-12-05 23:19:38 +0800

    commit

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,14 +1,8 @@
 return {
 	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			vim.opt.list = true
-			vim.opt.listchars:append("eol:↲")
-			require("indent_blankline").setup({
-				show_end_of_line = true,
-				space_char_blankline = " ",
-			})
-		end,
 		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
 	},
 }
