3ac072cf31433b17e3ea0a94fa804b8095535fb0
xiantang <zhujingdi1998@gmail.com>
2023-12-05 22:04:22 +0800

    commit

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,4 +1,19 @@
 vim.cmd([[
+function Undotree_record() abort
+if has("persistent_undo")
+	 let target_path = expand('~/.undodir')
+		" create the directory and any parent directories
+		" if the location does not exist.
+		if !isdirectory(target_path)
+				call mkdir(target_path, "p", 0700)
+		endif
+
+		let &undodir=target_path
+		set undofile
+endif
+endfunction
+let g:undotree_WindowLayout = 3
+au BufEnter * call Undotree_record()
 au TermOpen * setlocal nobuflisted
 au VimResized * wincmd =
 au BufEnter leetcode.cn_*.txt set filetype=go
