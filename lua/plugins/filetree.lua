87f8abd9c687eecf64469f43d5f2df5e0042f87c
xiantang <zhujingdi1998@gmail.com>
2023-12-10 22:59:38 +0800

    MiniFilesBufferCreate mini

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -35,6 +35,33 @@ return {
 					trim_right = ">",
 				},
 			})
+			local window_handle
+			local mini_files = require("mini.files")
+
+			local group = vim.api.nvim_create_augroup("mini.files", { clear = true })
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "MiniFilesBufferCreate",
+				desc = "define commands/keymaps for mini.files",
+				group = group,
+				callback = function(args)
+					window_handle = vim.api.nvim_get_current_win()
+					-- mini_files.open(vim.api.nvim_buf_get_name(0))
+					local buf_id = args.data.buf_id
+					vim.keymap.set("n", "s", function()
+						local entry = mini_files.get_fs_entry()
+						if not entry then
+							return
+						end
+						print(window_handle)
+						vim.api.nvim_win_call(window_handle, function()
+							vim.cmd.vsplit({ args = { entry.path } })
+						end)
+					end, {
+						buffer = buf_id,
+						desc = "open from mini.files in vertical split",
+					})
+				end,
+			})
 		end,
 	},
 }
