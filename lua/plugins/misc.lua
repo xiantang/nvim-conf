2b16711cfd3227bc8cf9cd6f678db29b6a4fff4d
xiantang <zhujingdi1998@gmail.com>
2024-02-03 18:18:55 +0800

    commit

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,5 +1,15 @@
 return {
 	{
+		"xiantang/nvim-macros",
+		cmd = { "MacroSave", "MacroYank", "MacroSelect", "MacroDelete" },
+		opts = {
+
+			json_file_path = vim.fs.normalize(vim.fn.stdpath("config") .. "/macros.json"), -- Location where the macros will be stored
+			default_macro_register = "q", -- Use as default register for :MacroYank and :MacroSave and :MacroSelect Raw functions
+			json_formatter = "none", -- can be "none" | "jq" | "yq" used to pretty print the json file (jq or yq must be installed!)
+		},
+	},
+	{
 		"jamessan/vim-gnupg",
 		ft = { "gpg", "pgp", "asc" },
 	},
