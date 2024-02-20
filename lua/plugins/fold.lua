2336e2d02c2f92dc31f55ff0beeaaf81f104caed
xiantang <zhujingdi1998@gmail.com>
2024-02-21 01:58:18 +0800

    commit

--- lua/plugins/fold.lua
+++ lua/plugins/fold.lua
@@ -55,8 +55,8 @@ return {
 					require("statuscol").setup({
 						relculright = true,
 						segments = {
-							{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
 							{ text = { "%s" }, click = "v:lua.ScSa" },
+							{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
 							{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
 						},
 					})
