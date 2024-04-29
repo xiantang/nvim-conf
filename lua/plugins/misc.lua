f7bdbbb2f2360ac3ce58c082d9743d952e9543bc
xiantang <zhujingdi1998@gmail.com>
2024-04-30 00:23:15 +0800

    commit

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -93,10 +93,41 @@ return {
 		wiki_link_func = "use_alias_only",
 		markdown_link_func = "use_alias_only",
 		opts = {
+			mappings = {
+				-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+				["gd"] = {
+					action = function()
+						return require("obsidian").util.gf_passthrough()
+					end,
+					opts = { noremap = false, expr = true, buffer = true },
+				},
+				["<Leader>gr"] = {
+					action = function()
+						return [[<cmd>ObsidianBacklinks<CR>]]
+					end,
+					opts = { noremap = false, expr = true, buffer = true },
+				},
+				-- Toggle check-boxes.
+				-- ["<leader>ch"] = {
+				-- 	action = function()
+				-- 		return require("obsidian").util.toggle_checkbox()
+				-- 	end,
+				-- 	opts = { buffer = true },
+				-- },
+				-- Smart action depending on context, either follow link or toggle checkbox.
+				-- ["<cr>"] = {
+				-- 	action = function()
+				-- 		return require("obsidian").util.smart_action()
+				-- 	end,
+				-- 	opts = { buffer = true, expr = true },
+				-- },
+			},
+
 			workspaces = {
 				{
 					name = "personal",
 					path = "~/vault",
+					-- ln -s "/Users/neo/Library/Mobile Documents/iCloud~md~obsidian/Documents/vault" vault
 				},
 			},
 
