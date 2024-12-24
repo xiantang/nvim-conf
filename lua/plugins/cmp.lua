9c2cd6870db77ca366bb14b9cb9fadd5422a08dc
xiantang <zhujingdi1998@gmail.com>
2024-12-24 12:07:34 +0800

    disable use_nvim_cmp_as_default

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -57,7 +57,7 @@ return {
 				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
 				-- Useful for when your theme doesn't support blink.cmp
 				-- will be removed in a future release
-				use_nvim_cmp_as_default = true,
+				use_nvim_cmp_as_default = false,
 				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 				-- Adjusts spacing to ensure icons are aligned
 				nerd_font_variant = "mono",
