91beeb1285f93d624dd9b12d6b04434614d18e6e
xiantang <zhujingdi1998@gmail.com>
2025-05-20 18:11:39 +0800

    refactor: remove hardtime.nvim plugin configuration
    Remove the hardtime.nvim plugin that restricted key usage in Neovim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -4,19 +4,6 @@ vim.g.codecompanion_auto_tool_mode = true
 
 return {
 	{
-		"m4xshen/hardtime.nvim",
-		lazy = false,
-		dependencies = { "MunifTanjim/nui.nvim" },
-		opts = {
-			restricted_keys = {
-				["h"] = false,
-				["j"] = false,
-				["k"] = false,
-				["l"] = false,
-			},
-		},
-	},
-	{
 		"ravitemer/mcphub.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
